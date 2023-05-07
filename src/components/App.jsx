333cb9979c34fe216e58f95083e1fbcf753926c2
Djoult <55453354+Djoult@users.noreply.github.com>
2023-05-07 21:24:20 +0300

    Moving to root file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,61 @@
-import { FeedBack } from './Feedback/Feedback';
+import { Component } from 'react';
+import Controls from 'components/Controls/Controls';
+import Statistics from 'components/Statistics/Statistic';
+import Section from 'components/Section/Section';
+import Notification from 'components/Notification/Notification';
 
-export const App = () => {
-  return <FeedBack />;
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const { good } = this.state;
+    if (total !== 0) {
+      return +((good / total) * 100).toFixed(2);
+    }
+    return 0;
+  };
+
+  onLeaveFeedBack = state => {
+    this.setState(prevState => ({
+      [state]: prevState[state] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    const total = this.countTotalFeedback();
+    const PositiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <>
+        <Section title={'Please leave feedback'}>
+          <Controls options={options} onLeaveFeedBack={this.onLeaveFeedBack} />
+        </Section>
+        <Section title={'Statistics'}>
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={PositiveFeedbackPercentage}
+            />
+          ) : (
+            <Notification message={'There is no feedback'} />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
